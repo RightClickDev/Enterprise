38fb6fa0af58dadaa000a33fb80af87597d46ef4
rightclickdev <mark.griffiths1983@gmail.com>
2016-02-19 21:23:31 +0000

    Added Configuration and Convert Helper

---
+++ RightClick.Enterprise/Helpers/ConfigurationHelper.cs
@@ -0,0 +1,17 @@
+﻿//------------------------------------------------------------------------------------------------- 
+// <copyright file="ConfigurationHelper.cs" company="PSP">
+//  Copyright (c) PSP. All rights reserved.
+// </copyright>
+//-------------------------------------------------------------------------------------------------
+
+namespace RightClick.Enterprise.Helpers
+{
+    using System;
+
+    /// <summary>
+    /// Configuration Helper
+    /// </summary>
+    public class ConfigurationHelper
+    {
+    }
+}
---
+++ RightClick.Enterprise/Helpers/ConvertHelper.cs
@@ -0,0 +1,44 @@
+﻿//------------------------------------------------------------------------------------------------- 
+// <copyright file="ConvertHelper.cs" company="PSP">
+//  Copyright (c) PSP. All rights reserved.
+// </copyright>
+//-------------------------------------------------------------------------------------------------
+
+namespace RightClick.Enterprise.Helpers
+{
+    /// <summary>
+    /// Convert Helper
+    /// </summary>
+    public class ConvertHelper
+    {
+        /// <summary>
+        /// Converts the bytes to megabytes.
+        /// </summary>
+        /// <param name="bytes">The bytes.</param>
+        /// <returns>Convert Bytes To Megabytes</returns>
+        public static double ConvertBytesToMegabytes(int bytes)
+        {
+            return (bytes / 1024f) / 1024f;
+        }
+
+        /// <summary>
+        /// Converts the bytes to kilobytes.
+        /// </summary>
+        /// <param name="bytes">The bytes.</param>
+        /// <returns>Convert Bytes To Kilobytes</returns>
+        public static double ConvertBytesToKilobytes(int bytes)
+        {
+            return bytes / 1024f;
+        }
+
+        /// <summary>
+        /// Converts the bytes to kilobytes as string.
+        /// </summary>
+        /// <param name="bytes">The bytes.</param>
+        /// <returns>Convert Bytes To Kilobytes As String</returns>
+        public static string ConvertBytesToKilobytesAsString(int bytes)
+        {
+            return string.Format("{0:N0}", bytes / 1024f);
+        }
+    }
+}
